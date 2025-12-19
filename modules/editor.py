b5e51f405a5ff5a7a04d6fc7dea107cdc743d029
prasenjit-hub <prasenjitchakraborty174@gmail.com>
2025-12-20 01:55:13 +0530

    Increased reaction video size to 28% width

--- modules/editor.py
+++ modules/editor.py
@@ -305,8 +305,8 @@ class VideoEditor:
         
         # 2. Add Reaction (if present)
         if has_reaction:
-            # Reaction size: 15% of width (roughly)
-            react_w = int(out_w * 0.15)
+            # Reaction size: 28% of width
+            react_w = int(out_w * 0.28)
             padding = 20
             
             # Process Reaction: [2:v] -> Crop Square -> Scale
